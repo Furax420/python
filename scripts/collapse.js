f8a53a51c420cf9523171eab75bd51ae32f4ff4e
Tristan <tristan.mayer96@gmail.com>
2024-01-21 23:39:36 +0100

    collapse + title0

--- scripts/collapse.js
+++ scripts/collapse.js
@@ -1,13 +1,12 @@
 document.querySelectorAll('.collapse-button').forEach(button => {
     button.addEventListener('click', function() {
-      const content = this.nextElementSibling;
-      const arrow = this.querySelector('.collapse-arrow');
-  
-      // Basculer la visibilité du contenu
-      content.style.display = content.style.display === 'block' ? 'none' : 'block';
-  
-      // Animation de la flèche
-      arrow.innerHTML = content.style.display === 'block' ? '&#9650;' : '&#9660;';
+        const content = this.nextElementSibling;
+        const arrow = this.querySelector('.collapse-arrow');
+
+        // Basculer la visibilité du contenu
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+
+        // Changer l'image de la flèche
+        arrow.src = content.style.display === 'block' ? '/sources/angle-de-la-fleche-vers-le-haut.png' : '/sources/fleche-vers-le-bas.png';
     });
-  });
-  
+});
